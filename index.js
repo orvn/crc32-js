181aeb6d2f175120a1ef45a2a01f844d2a9068cb
Orun Bhuiyan <orun@agencyundone.com>
2023-05-06 01:48:35 -0400

    feat: crc32 with polymonial division

---
+++ index.js
@@ -0,0 +1,28 @@
+/*!
+ * js-crc32 <https://github.com/orvn/js-crc32>
+ * Apache license
+ */
+
+'use strict';
+
+function crc32Polynomial(str) {
+    let crc = 0xFFFFFFFF;
+  
+    for (let i = 0, len = str.length; i < len; i++) {
+      let byte = str.charCodeAt(i);
+      crc ^= byte;
+  
+      for (let j = 0; j < 8; j++) {
+        const mask = -(crc & 1);
+        crc = (crc >>> 1) ^ (0xEDB88320 & mask);
+      }
+    }
+  
+    crc = (crc ^ 0xFFFFFFFF) >>> 0;
+  
+    // Convert to little endian byte order
+    crc = ((crc & 0xFF) << 24) | (((crc >>> 8) & 0xFF) << 16) | (((crc >>> 16) & 0xFF) << 8) | ((crc >>> 24) & 0xFF);
+  
+    // Properly handle the conversion of the CRC checksum to a hexadecimal string
+    return (crc >>> 0).toString(16).padStart(8, '0');
+  }
---
+++ test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require('mocha');
+const assert = require('assert');
+const crc32 = require('./');
