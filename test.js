fa20bf684c93e50f33a196751cab170b9767abfd
Orun Bhuiyan <orun@agencyundone.com>
2023-05-08 00:37:18 -0400

    fix: tests

--- test.js
+++ test.js
@@ -2,4 +2,11 @@
 
 require('mocha');
 const assert = require('assert');
-const crc32 = require('./');
+const crc32 = require('./index.js');
+
+describe('crc32', function() {
+    it('Returns specific value for CRC32 of a string foo:', function() {
+      let result = crc32('foo');
+      assert.equal(result, "2165738c");
+    });
+  });
