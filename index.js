37a6e2c2275d99ada9c7801def3be0abda6d966c
Orun Bhuiyan <orun@agencyundone.com>
2023-05-16 00:42:46 -0400

    feat: option to output in decimal or hex

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function crc32_polynomialDivision(str) {
         crc = (crc >>> 1) ^ (polynomial & mask);
     }
     }
-    // Handle conversion to hexadecimal string
-    return ((crc ^ 0xFFFFFFFF) >>> 0).toString(16).padStart(8, '0');
+    return (crc ^ 0xFFFFFFFF) >>> 0;
 }
 
 // Lookup table implementation (this is the default because it's faster, due to being a precalculated table)
@@ -31,15 +30,16 @@ function crc32_lookupTable(str) {
     crc = (crc >>> 8) ^ crc32Table[(crc ^ str.charCodeAt(i)) & 0xFF];
     }
 
-    return ((crc ^ 0xFFFFFFFF) >>> 0).toString(16).padStart(8, '0');
+    return (crc ^ 0xFFFFFFFF) >>> 0;
 }
 
 // Export complete crc32 function
-
-module.exports = function crc32(str, usePolynomialDivision = false) {
+module.exports = function crc32(str, usePolynomialDivision = false, hexFormat = true) {
+    let crc;
     if (usePolynomialDivision) {
-    return crc32_polynomialDivision(str);
+    crc = crc32_polynomialDivision(str);
     } else {
-    return crc32_lookupTable(str);
+    crc = crc32_lookupTable(str);
     }
+    return hexFormat ? crc.toString(16).padStart(8, '0') : crc.toString(10);
 }
